851e5ea49fe62783757abbd24598f64beb8371a1
BrunoBianchi <Bruno.raiadobian@gmail.com>
2024-01-29 18:47:01 -0300

    ENV

--- src/app/lib/mail.js
+++ src/app/lib/mail.js
@@ -1,9 +1,9 @@
 import nodemailer from 'nodemailer';
 export default nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    host: "",
+    port: "",
     auth: {
-      user: "9924767038e08f",
-      pass: "b6730203d32048"
+      user: "",
+      pass: ""
     }
 })
